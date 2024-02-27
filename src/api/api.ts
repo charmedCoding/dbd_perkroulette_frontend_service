7de4ef578453a1ef01d067ea563adb6b0519bae4
charmedCoding <ellierulez2000@gmail.com>
2024-02-27 18:29:51 +0100

    styling anpassung

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,4 @@
 import axios from 'axios'
 
-export default axios.create({baseURL: "http://35.198.87.180:8081/api/"})
+// export default axios.create({baseURL: "http://35.198.87.180:8081/api/"})
+export default axios.create({baseURL: "http://localhost:8081/api/"})
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,20 +4,38 @@ import survivor_perks from '../utils/survivor_perks.json'
 import {usePerks} from '../features/perks/PerkApi'
 import {getRandomElements} from '../utils/Randomizer'
 import {Button} from "@mui/material"
+import Tooltip from '@mui/material/Tooltip';
 
 
 
 const MainPage: React.FunctionComponent = () => {
 const [fourPerks, setFourPerks] = useState([])
+const [perkInfo, setPerkInfo] = useState([])
+const [name, setName] = useState([])
+const [description, setDescription] = useState([])
+const [character, setCharacter] = useState([])
+
     const perkQuery = usePerks()
     const perkList = perkQuery.data
 
+    const setPerk = (item: any) => {
+        setName(item.name);
+        setDescription(item.description)
+        setCharacter(item.character)
+    }
+
+    const givePerkDescription = (item:any) => {
+setPerkInfo(item.description)
+    }
 
     const newPerksOnClick= () => {
+ setPerkInfo([]);
         if(perkList){
         setFourPerks(getRandomElements(perkList,4))
     }
 }
+
+
      dataCleanser(survivor_perks)
 
      if (perkQuery.isLoading){
@@ -34,22 +52,31 @@ const [fourPerks, setFourPerks] = useState([])
     return(
     <>
    
+    
+        <div className = "main"> 
         <h1> Dead by Daylight Perkroulette</h1>
-        <div>
-            {/* <img src = "https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/f/f7/IconPerks_adrenaline.png" ></img>
-            <img src = "https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/2/24/IconPerks_aceInTheHole.png" alt="Description"/> */}
-           
-            <div>
-                
+            <div className = "perkroulette">
+                <div className = "fourperks">
                 {fourPerks.map((perk:any) => (
-                    <div className = "perks">
-                    <p key={perk.id}>{perk.name} </p>
+                    <div className = "perks"key={perk.id}>
+                    <p >{perk.name} </p>
+                    <>
+                    <Tooltip title={description} arrow>
+                       
+                    <Button className = "perk-button" onClick = {() => setPerk(perk)}> 
                     <img src = {perk.img_url}/>
+                    </Button>
+                    
+                    </Tooltip>
+                    <p >{perk.character} </p>
+                    </>
                    </div>
               
-                ))}
+                ))} 
+                </div>
+              
             </div>
-            <Button onClick={newPerksOnClick}> Randomize Perks </Button>
+            <Button className= "generator-button" onClick={newPerksOnClick}> Randomize Perks </Button>
         </div>
        
         </>
