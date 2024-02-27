921434586a73dbb622d8914b041738131a1e9d10
charmedCoding <ellierulez2000@gmail.com>
2024-02-27 22:35:53 +0100

    design

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,14 +6,21 @@ import {getRandomElements} from '../utils/Randomizer'
 import {Button} from "@mui/material"
 import Tooltip from '@mui/material/Tooltip';
 
-
-
 const MainPage: React.FunctionComponent = () => {
-const [fourPerks, setFourPerks] = useState([])
+    const meg_url = "https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/1/12/SurvivorMeg.png";
+    const locker_url = "https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/d/de/Dbd-survivor-closet.png"
+    const hatch_url = "https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/f/fc/IconHelpLoading_hatch.png"
+    const survivor_url = "https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/b/b3/IconHelpLoading_survivor.png"
+    const totem_url = "https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/e/e9/IconHelpLoading_totem.png"
+    const perk_base_url = "https://static.wikia.nocookie.net/deadbydaylight_gamepedia_en/images/7/74/IconHelp_perks.png"
+    const perkStart: any = {name: "", description: "", character:"", img_url: survivor_url}
+const [fourPerks, setFourPerks] = useState([perkStart, perkStart, perkStart,perkStart])
 const [perkInfo, setPerkInfo] = useState([])
 const [name, setName] = useState([])
 const [description, setDescription] = useState([])
 const [character, setCharacter] = useState([])
+
+
 
     const perkQuery = usePerks()
     const perkList = perkQuery.data
@@ -54,19 +61,18 @@ setPerkInfo(item.description)
    
     
         <div className = "main"> 
-        <h1> Dead by Daylight Perkroulette</h1>
+        
             <div className = "perkroulette">
+            <h1> Dead by Daylight Perkroulette</h1>
                 <div className = "fourperks">
                 {fourPerks.map((perk:any) => (
                     <div className = "perks"key={perk.id}>
                     <p >{perk.name} </p>
                     <>
                     <Tooltip title={description} arrow>
-                       
                     <Button className = "perk-button" onClick = {() => setPerk(perk)}> 
-                    <img src = {perk.img_url}/>
+                    <img src={perk.img_url} className="center-image" />
                     </Button>
-                    
                     </Tooltip>
                     <p >{perk.character} </p>
                     </>
@@ -74,9 +80,9 @@ setPerkInfo(item.description)
               
                 ))} 
                 </div>
-              
+                <Button variant="contained" className= "generator-button" style={{backgroundColor: 'orange', color: 'white'}} onClick={newPerksOnClick}> Randomize Perks </Button>
             </div>
-            <Button className= "generator-button" onClick={newPerksOnClick}> Randomize Perks </Button>
+            
         </div>
        
         </>
